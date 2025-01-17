5e56d27240ac40c920e3307105eba23710d614db
Ashish Singh <ashishsinghrana39@gmail.com>
2025-01-17 13:03:16 +0530

    committed

--- Main.java
+++ Main.java
@@ -4,7 +4,7 @@ public class Main{
         Scanner sc=new Scanner(System.in)
         int a=sc.nextInt();
         int b=sc.nextInt();
-        int c=a+b;
-        System.out.println(c);
+       
+        System.out.println(a+b);
     }
 }
